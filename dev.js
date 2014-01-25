6f9443629e0dbe4eea93cae0c2622474e35f90ba
Felix Storm <felix.storm@glueckkanja.com>
2014-01-25 17:47:50 +0100

    added development version

---
+++ dev.js
@@ -0,0 +1,77 @@
+"use strict";
+
+var defaultPlayer = "Bad";
+
+var actions = {
+  "KEY_VOLUMEUP": function (player) { player.setVolume("+1"); return true; },
+  "KEY_VOLUMEDOWN": function (player) { player.setVolume("-1"); return true; },
+  "165": function (player) { player.coordinator.previousTrack(); },
+  "163": function (player) { player.coordinator.nextTrack(); },
+  "164": function (player) { player.coordinator.play(); },
+  "166": function (player) { player.coordinator.pause(); }
+};
+
+var actionsFavorites = {
+  "KEY_KP0": "0 ",
+  "KEY_KP1": "1 ",
+  "KEY_KP2": "2 ",
+  "KEY_KP3": "3",
+  "KEY_KP4": "4 ",
+  "KEY_KP5": "5 ",
+  "KEY_KP6": "6 ",
+  "KEY_KP7": "7 ",
+  "KEY_KP8": "8 ",
+  "KEY_KP9": "9 ",
+};
+
+var actionsSwitchPlayer = {
+  "KEY_T": "Wohnzimmer",
+  "KEY_M": "Bad",
+  "KEY_I": "Kueche"
+};
+// player = discovery.getPlayer("Wohnzimmer");
+
+// presets for grouping
+var presets = {
+  "all": {"players": [
+    { "roomName": "Wohnzimmer", "volume": 10 },
+    { "roomName": "Bad", "volume": 10 },
+    { "roomName": "Kueche", "volume": 10 }
+  ]}};
+
+// this maps keycodes to predefined presets
+var buttonToPreset = {
+  "1a": "all",
+  "2a": "tv"
+};
+
+
+var SonosDiscovery = require('sonos-discovery');
+var discovery = new SonosDiscovery();
+
+var player = null;
+discovery.on('topology-change', function () {
+  if (player == null) {
+    player = discovery.getPlayer(defaultPlayer);
+  }
+});
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function(line) {
+  line = line.trim();
+  console.log('data: ' + line);
+  
+  var action = actions[line];
+  if (player && action) {
+    action(player);
+  }
+  
+  var actionFavorite = actionsFavorites[line];
+  if (player && actionFavorite) {
+    player.replaceWithFavorite(actionFavorite, function() {
+      player.play();
+    });
+  }
+});
